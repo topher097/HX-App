a98fabeec7c9277fdfaf588f6d4be68003e4990a
topher097 <cmendres400@gmail.com>
2021-07-07 09:37:51 -0400

    PID tuner app

--- saveConfigurationFile.m
+++ saveConfigurationFile.m
@@ -1,9 +1,11 @@
+load configuration.mat
+
 % Variables
 BAUD            = 115200;
 comPort         = 'COM6';
 plotSeconds     = 30;
 plotUpdateTime  = 0.5;
-readVarCount    = 27;
+readVarCount    = 30;
 readVarType     = "string";
 sendVarCount    = 13;
 sendVarType     = "double";
@@ -11,6 +13,11 @@ sendVarType     = "double";
 % Save configuration.mat file
 save('configuration.mat', 'BAUD', 'comPort', 'plotSeconds', 'plotUpdateTime', 'readVarCount', ...
      'readVarType', 'sendVarCount', 'sendVarType', '-v7.3');
+Kp = 1;
+Kd = 0.5;
+Ki = 0;
+save('PIDvalues.mat', 'Kp', 'Ki', 'Kd', '-V7.3');
+
 clc;
 clear;
 home;
